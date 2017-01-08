7b24694608b7fc2aaf67ee108472a3075214bd20
Richard Kombol <terungwakombol@gmail.com>
2017-01-08 12:02:20 +0100

    data-type function: test case for boolean. Test spec for true

--- andela-labs/data-types-lab/spec/librarySpec.js
+++ andela-labs/data-types-lab/spec/librarySpec.js
@@ -5,10 +5,18 @@ describe("Data types tests ", function() {
       expect(dataTypes(null)).toEqual('no value');
     });
 
-  });
-
-  it("should return 'no value' for undefined", function() {
+    it("should return 'no value' for undefined", function() {
       expect(dataTypes(undefined)).toEqual('no value');
     });
+
+  });
+
+  describe("Case for booleans", function() {
+
+    it("should return true for `true`", function() {
+      expect(dataTypes(true)).toBe(true);
+    });
+
+  });
 
 });
