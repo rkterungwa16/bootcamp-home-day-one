b149994580c8c78a9f0844805c8b07505dccf7c2
Richard Kombol <terungwakombol@gmail.com>
2017-01-08 13:16:52 +0100

    get-prime function: Test case for non numerical input and numerical string passes

--- andela-labs/get-primes-lab/tests/tests.js
+++ andela-labs/get-primes-lab/tests/tests.js
@@ -12,4 +12,8 @@ describe("Case for non numerical inputs", function() {
     expect(myApp.getPrimes([])).to.equal('wrong input type');
   }); 
 
+  it("should return 'wrong input type' for '5'" , function() {
+    expect(myApp.getPrimes('5')).to.equal('wrong input type');
+  });
+
 });
