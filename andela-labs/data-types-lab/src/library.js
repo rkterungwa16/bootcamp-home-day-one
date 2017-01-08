0a43ee12a27ffaf1c7da6318de760ee7e992bf11
Richard Kombol <terungwakombol@gmail.com>
2017-01-08 12:13:18 +0100

    data-type function: Test case for numbers all pass.

--- andela-labs/data-types-lab/src/library.js
+++ andela-labs/data-types-lab/src/library.js
@@ -6,4 +6,15 @@ function dataTypes(value) {
    else if (typeof value === 'boolean') {
       return value;
    }
+
+   else if (typeof value === 'number' && value < 100) {
+      return 'less than 100';
+   }
+
+   else if (typeof value === 'number' && value > 100) {
+      return 'more than 100';
+   }
+   else if (typeof value === 'number' && value === 100) {
+      return 'equal to 100';
+   }
 }
