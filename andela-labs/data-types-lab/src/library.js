d59904a6b84be2d75550383a5763efdb89bbc6d3
Richard Kombol <terungwakombol@gmail.com>
2017-01-08 11:54:20 +0100

    Modified Jasmine. First two test for undefined and null input values fail

--- andela-labs/data-types-lab/src/library.js
+++ andela-labs/data-types-lab/src/library.js
@@ -1,32 +1,4 @@
 
 function dataTypes(value) {
-
-   if (value === null || value === undefined) {
-      return 'no value';
-   }
-   if (typeof value === 'string') {
-      return value.length;
-   }
    
-   else if (typeof value === 'boolean') {
-      return value;
-   }
-
-   else if (typeof value === 'number' && value < 100) {
-      return 'less than 100';
-   }
-
-   else if (typeof value === 'number' && value > 100) {
-      return 'more than 100';
-   }
-   else if (typeof value === 'number' && value === 100) {
-      return 'equal to 100';
-   }
-
-   else if (value instanceof Array && value.length > 2) {
-      return value[2];
-   }
-   else if (typeof value === 'function'){
-      return 'called callback';
-   }
 }
