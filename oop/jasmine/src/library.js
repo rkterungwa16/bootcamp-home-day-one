4a287d1b58c271e147143aac0fd538d4534059ee
Richard Kombol <terungwakombol@gmail.com>
2017-01-09 08:22:45 +0100

    School Object: test passes for university private attributes

--- oop/jasmine/src/library.js
+++ oop/jasmine/src/library.js
@@ -10,9 +10,19 @@ var school = function(name, localGovt, state) {
 
 
 var university = function(name, localGovt, state, nameOfStreet) {
+	// Private members
+   var _programmes = {
+      undergrad: 'Undergraduate',
+      postgrad: 'Postgraduate'
+   }
+
    var that = school(name, localGovt, state);
    that.nameOfStreet = nameOfStreet || '';
    that.headOfInstitution = 'Vice Chancellor';
+
+   that.returnProgrammes = function() {
+      return _programmes;
+   }
 
    return that;
 }
